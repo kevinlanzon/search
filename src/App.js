99aba780e5f4a73b25322c0e9ee2953a9dcb2006
Kevin Lanzon <kevinlanzon@gmail.com>
2017-12-22 18:09:28 +0000

    Adds search bar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SearchBar from './components/searchBar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Search</h1>
+          <div>
+            <SearchBar />
+          </div>
         </header>
       </div>
     );
